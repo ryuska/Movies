86bc0847028ca2de8760de261ebc9cb2197b94ac
ryuska <yromanova2010@gmail.com>
2019-04-25 17:01:56 -0400

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,15 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> movies = new ArrayList<>();
+        int num;
+
 
         String movieName = "";
         Scanner input = new Scanner(System.in);
@@ -18,6 +22,8 @@ public class Main {
             System.out.println(movies);
             System.out.println("Please enter a movie name: ");
             movieName = input.nextLine();
+            Collections.sort(movies);
+
         }
 
         if (movieName.equalsIgnoreCase ("q")){
@@ -27,6 +33,11 @@ public class Main {
         else {
             movies.add(movieName);
             System.out.println(movies);
+
         }
+        Random rnd =new Random();
+        int randomIndex = rnd.nextInt(movies.size());
+        System.out.println("Here is movie suggesting for tonight: ");
+        System.out.println(movies.get(randomIndex));
     }
 }
